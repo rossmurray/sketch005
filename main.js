d4c48aafa2c8fb4aa7bcd1068d694f9c16034bc4
rossmurray <rm@egoorb.com>
2017-11-16 20:56:04 -0600

    partially rewritten hex drawing functions

--- main.js
+++ main.js
@@ -14,8 +14,10 @@ var fnMain = (function() {
         const pstring = '#DDDF99,#22DD22,navy,plum';
         const palette = pstring.split(',');
         return {
-            shapeRadius: 0.08,
+            shapeRadius: 0.08, //relative to board
             shapeHolePercent: 1.01,
+            hexOuterBorderWidth: 0.1, //percent of shape diameter
+            hexInnerBorderWidth: 0.04,
             shrinkPercent: 0.7,
             animationDuration: 1300,
             animationOffset: 1.5,
@@ -26,7 +28,9 @@ var fnMain = (function() {
             shapeAlpha: 1,
             shapeBlendMode: PIXI.BLEND_MODES.NORMAL,
             palette: palette,
-            backgroundColor: 0x0,
+            backgroundColor: colorNameToNumber('black'),
+            hexInnerBorderColor: 'black',
+            hexQuadColors: ['black','white','gray'],
         };
     }
 
@@ -48,6 +52,14 @@ var fnMain = (function() {
             | Math.floor(rgbArray[1]) << 8
             | Math.floor(rgbArray[0]) << 16;
         return result;
+    }
+
+    function colorNameToNumber(name) {
+        return RGBTo24bit(chroma(name).rgb());
+    }
+
+    function colorNumberToName(number) {
+        return chroma(number).name();
     }
 
     function portion(i, size) {
@@ -72,6 +84,69 @@ var fnMain = (function() {
         const texture = PIXI.Texture.fromCanvas(canvasElement);
         const sprite = new PIXI.Sprite(texture);
         return sprite;
+    }
+
+    function createHexagonTexture(config, renderer) {
+        const radius = config.shapeRadius;
+        const diameter = radius * 2;
+        const g = new PIXI.Graphics();
+        g.width = diameter;
+        g.height = diameter;
+        const innerPoints = getHexagonPoints(radius, radius, radius);
+        const outerPoints = getHexagonPoints(radius, radius, radius);
+        function getQuad(n) {
+            const mid = [new PIXI.Point(radius, radius)];
+            const edges = [0,1,2].map(x => (x + 2 * n) % 6).map(x => outerPoints[x]);
+            return mid.concat(edges);
+        }
+        //draw quad 0
+        g.beginFill(colorNameToNumber(config.hexQuadColors[0]), config.shapeAlpha);
+        const quad0 = getQuad(0);
+        g.drawPolygon(quad0);
+        g.endFill();
+
+        //draw quad 1
+        g.beginFill(colorNameToNumber(config.hexQuadColors[1]), config.shapeAlpha);
+        const quad1 = getQuad(1);
+        g.drawPolygon(quad1);
+        g.endFill();
+
+        //draw quad 2
+        g.beginFill(colorNameToNumber(config.hexQuadColors[2]), config.shapeAlpha);
+        const quad2 = getQuad(2);
+        g.drawPolygon(quad2);
+        g.endFill();
+
+        const innerBorderWidth = Math.round(config.hexInnerBorderWidth * diameter);
+        g.lineStyle(innerBorderWidth, colorNameToNumber(config.hexInnerBorderColor));
+        g.moveTo(radius, radius);
+        g.lineTo(outerPoints[0].x, outerPoints[0].y)
+        g.moveTo(radius, radius);
+        g.lineTo(outerPoints[2].x, outerPoints[2].y)
+        g.moveTo(radius, radius);
+        g.lineTo(outerPoints[4].x, outerPoints[4].y)
+        g.lineStyle(0);
+
+        const smallerRadius = Math.round(config.shapeRadius * config.shapeHolePercent);
+        //const polygonPoints = drawHexagon(g, config.shapeRadius, config.shapeRadius, config.shapeRadius, 0x0, config.shapeAlpha);
+        //drawHexagon(g, config.shapeRadius, config.shapeRadius, smallerRadius, config.backgroundColor, config.shapeAlpha);
+        const texture = PIXI.RenderTexture.create(diameter, diameter);
+        renderer.render(g, texture);
+        return texture;
+    }
+
+    function getHexagonPoints(centerX, centerY, radius) {
+        const sides = 6;
+        const points = makeRange(sides).map((x,i) => {
+            const amountAround = i / sides;
+            const vx = radius * Math.cos(Math.PI * 2 * amountAround) + centerX;
+            const vy = radius * Math.sin(Math.PI * 2 * amountAround) + centerY;
+            const point = new PIXI.Point(Math.round(vx), Math.round(vy));
+            return point;
+        });
+        points[2].x = points[4].x;
+        //todo adjust more
+        return points;
     }
 
     function drawHexagon(graphics, centerX, centerY, radius, color24, alpha) {
@@ -105,34 +180,37 @@ var fnMain = (function() {
         const testPoints = drawHexagon(new PIXI.Graphics(), config.shapeRadius, config.shapeRadius, config.shapeRadius, config.backgroundColor, 1);
         const shapeWidth = testPoints[2].y - testPoints[4].y;
         const shapeHeight = (diameter + (testPoints[1].x - testPoints[2].x)) / 2;
-        const colCount = Math.ceil(board.width / shapeWidth) + 1;
-        const rowCount = Math.ceil(board.height / shapeHeight) + 1;
+        const colCount = Math.ceil(board.width / shapeWidth) + 2;
+        const rowCount = Math.ceil(board.height / shapeHeight) + 2;
         const shapeCount = colCount * rowCount;
         const diagDist = function(j,k) {
             return (portion(j,colCount) + portion(k,rowCount)) / 2;
         };
         const shapes = makeRange(shapeCount).map(() => {return {};});
+        const texture = createHexagonTexture(config, renderer);
         for(let j = 0; j < colCount; j++) { //columns
             for(let k = 0; k < rowCount; k++) { //rows
                 const i = k + rowCount * j;
                 const shape = shapes[i];
-                const g = new PIXI.Graphics();
-                g.width = diameter;
-                g.height = diameter;
-                const color = RGBTo24bit(config.colorScale(diagDist(j,k)).rgb());
-                const smallerRadius = Math.round(config.shapeRadius * config.shapeHolePercent);
-                const polygonPoints = drawHexagon(g, config.shapeRadius, config.shapeRadius, config.shapeRadius, 0x0, config.shapeAlpha);
-                drawHexagon(g, config.shapeRadius, config.shapeRadius, smallerRadius, config.backgroundColor, config.shapeAlpha);
-                const texture = PIXI.RenderTexture.create(diameter, diameter);
-                renderer.render(g, texture);
+                // const g = new PIXI.Graphics();
+                // g.width = diameter;
+                // g.height = diameter;
+                // const color = RGBTo24bit(config.colorScale(diagDist(j,k)).rgb());
+                // const smallerRadius = Math.round(config.shapeRadius * config.shapeHolePercent);
+                // const polygonPoints = drawHexagon(g, config.shapeRadius, config.shapeRadius, config.shapeRadius, 0x0, config.shapeAlpha);
+                // drawHexagon(g, config.shapeRadius, config.shapeRadius, smallerRadius, config.backgroundColor, config.shapeAlpha);
+                // const texture = PIXI.RenderTexture.create(diameter, diameter);
+                // renderer.render(g, texture);
                 const sprite = new PIXI.Sprite(texture);
                 const evenRow = (k % 2 == 0);
-                const rowShift = evenRow ? 0 : shapeWidth * -0.5;
+                const rowShift = evenRow ? -0.4 * shapeWidth : shapeWidth * -0.9;
+                const colShift = shapeHeight * -0.4;
+                const rotShift = 0;//((i % 3) / 3) * Math.PI * 2;
                 sprite.x = rowShift + board.left + j * shapeWidth;
-                sprite.y = board.top + k * shapeHeight;
+                sprite.y = colShift + board.top + k * shapeHeight;
                 sprite.anchor.set(0.5, 0.5);
                 sprite.blendMode = config.shapeBlendMode;
-                sprite.rotation = Math.PI * 0.5;
+                sprite.rotation = Math.PI * 0.5 + rotShift;
                 shape.sprite = sprite;
             }
         }
